1c6ab2f2a846639604147068a67aa1c26c5dc4a7
Taine Fan <fty221@gmail.com>
2025-03-14 17:04:27 +0800

    Add tool for ast generator

---
+++ com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+abstract class Expr {
+
+    static class Binary extends Expr {
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
+        public Binary(Expr left, Token operator, Expr right) {
+          this.left = left;
+          this.operator = operator;
+          this.right = right;
+        }
+
+
+
+    }
+}
